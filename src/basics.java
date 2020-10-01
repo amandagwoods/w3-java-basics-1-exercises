4f32b67b30e0d20834691feffe81614cd267a695
Amanda Woods <amanda.garrison.woods@gmail.com>
2020-09-30 22:51:39 -0500

    completed basics 1 & 2

---
+++ src/basics.java
@@ -0,0 +1,11 @@
+public class basics {
+    public static void main(String[] args){
+// 1
+        System.out.println("Hello");
+        System.out.println("Amanda Woods");
+
+// 2
+        int myNumber = 74+36;
+        System.out.println(myNumber);
+    }
+}
