70d969db85aa27bef1bd43be26d9900f247551bc
Amanda Woods <amanda.garrison.woods@gmail.com>
2020-09-30 22:56:36 -0500

    completed 3 & 4

--- src/basics.java
+++ src/basics.java
@@ -7,5 +7,19 @@ public class basics {
 // 2
         int myNumber = 74+36;
         System.out.println(myNumber);
+
+// 3
+        System.out.println(50/3);
+
+// 4
+//        a
+        System.out.println(-5 + 8 * 6);
+//        b
+        System.out.println((55+9) % 9);
+//        c
+        System.out.println(20 + -3*5 / 8);
+//        d
+        System.out.println(5 + 15 / 3 * 2 - 8 % 3);
+
     }
 }
